7e8b19787803098fc5d20e7c4f2f30f66c648c13
vrdaavid <david0203@estudiantec.cr>
2017-11-13 22:44:22 -0600

    TEST COMPI

---
+++ test.c
@@ -0,0 +1,10 @@
+int main()
+{
+	int a;
+
+	for (int i = 0; j < 5; i++)
+	{
+		a = i;
+	}
+				
+}
---
+++ test2.c
@@ -0,0 +1,17 @@
+int main ()
+{	
+	int X; 
+	switch(x){ 
+
+		case 2:
+			x = 1;
+
+		case 10:
+			x = 2;
+
+		default:
+			x = 10;
+	}
+	int c = 5;
+}
+
